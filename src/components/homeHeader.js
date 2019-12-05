4d373a8b0b6b54bb174a318164861f1fea761ca0
Luciekimotho <luciekimotho@gmail.com>
2019-12-05 18:11:54 +0300

    Added home page navbar icons

--- src/components/homeHeader.js
+++ src/components/homeHeader.js
@@ -22,7 +22,7 @@ class HomeHeader extends React.Component{
     render(){
         return (
             <> 
-            <header className="header-global">
+            <header className="header-global home-header">
                 <Navbar className="navbar-main navbar-transparent navbar-light headroom" expand="lg" id="navbar-main">
                     <div className="container-fluid">
                         <NavbarBrand>
@@ -56,49 +56,51 @@ class HomeHeader extends React.Component{
                             <Nav className="ml-lg-auto" navbar>
                                 <NavItem>
                                     <Link to="/" className="btn btn-warning btn-icon text-white">
-                                        <span className="btn-inner--icon"><i className="fa fa-home mr-2" /></span>
-                                            <br></br>
+                                        <span className="btn-inner--icon">
+                                            <img className="icon" alt=".." src={require('../assets/img/home_icons/Home.png')}></img>
+                                        </span>
+                                        <br></br>
                                         <span className="nav-link-inner--text ml-1">Home</span>
                                     </Link>
                                 </NavItem>
                                 <NavItem>
                                     <Link to="/Sdgs" className="btn btn-warning btn-icon text-white">
-                                        <span className="btn-inner--icon"><i className="fa fa-cloud-download mr-2" /></span>
+                                        <img className="icon" alt=".." src={require('../assets/img/home_icons/Sdg.png')}></img>
                                         <br></br>
                                         <span className="nav-link-inner--text ml-1">SDGS</span>
                                     </Link>
                                 </NavItem>
                                 <NavItem>
                                     <Link to="/Dashboard" className="btn btn-warning btn-icon text-white">
-                                            <span className="btn-inner--icon"><i className="fa fa-table mr-2" /></span>
-                                            <br></br>
-                                            <span className="nav-link-inner--text ml-1">Dashboard</span>
+                                        <img className="icon" alt=".." src={require('../assets/img/home_icons/Dashboard.png')}></img>
+                                        <br></br>
+                                        <span className="nav-link-inner--text ml-1">Dashboard</span>
                                     </Link>
                                 </NavItem>
                                 <NavItem>
                                     <Link to="/CountryProfile" className="btn btn-warning btn-icon text-white">
-                                    <span className="btn-inner--icon"><i className="fa fa-flag mr-2" /></span>
+                                        <img className="icon" alt=".." src={require('../assets/img/home_icons/CountryProfile.png')}></img>
                                             <br></br>
                                             <span className="nav-link-inner--text ml-1">Country <br></br> Profiles</span>
                                     </Link>
                                 </NavItem>
                                 <NavItem>
                                     <Link to="/Agenda2063" className="btn btn-warning btn-icon text-white">
-                                        <span className="btn-inner--icon"><i className="fa fa-globe mr-2" /></span>
+                                        <img className="icon" alt=".." src={require('../assets/img/home_icons/A2063.png')}></img>
                                         <br></br>
                                         <span className="nav-link-inner--text ml-1">Agenda <br></br> 2063</span>
                                     </Link>
                                 </NavItem>
                                 <NavItem>
                                     <Link to="/About" className="btn btn-warning btn-icon text-white">
-                                        <span className="btn-inner--icon"><i className="fa fa-info mr-2" /></span>
+                                        <img className="icon" alt=".." src={require('../assets/img/home_icons/AboutUs.png')}></img>
                                         <br></br>
                                         <span className="nav-link-inner--text ml-1">About us</span>
                                     </Link>
                                 </NavItem>
                                 <NavItem>
                                     <Link to="/Faqs" className="btn btn-warning btn-icon text-white">
-                                        <span className="btn-inner--icon"><i className="fa fa-question mr-2" /></span>
+                                        <img className="icon" alt=".." src={require('../assets/img/home_icons/FAQs.png')}></img>
                                         <br></br>
                                         <span className="nav-link-inner--text ml-1">FAQS</span>
                                     </Link>
--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/homeHeader";
 import Footer from "../components/footer";
 import { BrowserRouter, Link} from 'react-router-dom'
-import { Container, Col, Row } from "reactstrap";
+import { Container, Col, Row, Input, InputGroupAddon, InputGroupText, InputGroup} from "reactstrap";
 
 
 class Home extends React.Component {
@@ -11,21 +11,31 @@ class Home extends React.Component {
             <>
             <Header></Header>
                 <main className="home">
-                   <Container className="homeText">
-                       <h2 className="display-2 text-white">
-                       Africa SDG Watch is a public platform to visualize and explore data, benchmark progress towards the 
-                       SDGs and track performance of development indicators.
-                       </h2>
-                    </Container> 
-                    <Row>
-                        <Col></Col>
-                        <Col>
-                            <Link to="/Sdgs/Sdg_1" className="btn btn-warning text-white">Explore Development Data</Link>
-                        </Col>
-                        <Col>
-                            
-                        </Col>
-                    </Row>
+                    <div className="homeOverLay">
+                        <Container className="homeText">
+                        <h2 className="display-2 text-white">
+                        Africa SDG Watch is a public platform to visualize and explore data, benchmark progress towards the 
+                        SDGs and track performance of development indicators.
+                        </h2>
+                        </Container> 
+                        <Row>
+                            <Col md="4"></Col>
+                            <Col md="4">
+                                <Link to="/Sdgs/Sdg_1" className="btn btn-warning text-white">Explore Development Data</Link>
+                            </Col>
+                            <Col md="3">
+                                <InputGroup className="mb-4">
+                                    <InputGroupAddon addonType="prepend">
+                                        <InputGroupText>
+                                        <i className="ni ni-zoom-split-in" />
+                                        </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input placeholder="Search by country" type="text" />
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                    </div>
+                   
                 </main>
               
             </>
