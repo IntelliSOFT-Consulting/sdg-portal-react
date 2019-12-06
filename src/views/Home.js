a2f578db34719207e013b37d9fd415c8dd96793d
Luciekimotho <luciekimotho@gmail.com>
2019-12-06 15:03:33 +0300

    Added country search on home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,15 +3,32 @@ import Header from "../components/homeHeader";
 import Footer from "../components/footer";
 import { BrowserRouter, Link} from 'react-router-dom'
 import { Container, Col, Row, Input, InputGroupAddon, InputGroupText, InputGroup} from "reactstrap";
+import Select from 'react-select';
 
 
 class Home extends React.Component {
+    
     render(){
+        const countriesJson = require('../assets/data/trial.json');
+        const countries = countriesJson.map(country => ({ label: country.name, value: country.value }))
+
+
+        const scaryAnimals = [
+            { label: "Alligators", value: 1 },
+            { label: "Crocodiles", value: 2 },
+            { label: "Sharks", value: 3 },
+            { label: "Small crocodiles", value: 4 },
+            { label: "Smallest crocodiles", value: 5 },
+            { label: "Snakes", value: 6 },
+          ];  
+
         return(
             <>
             <Header></Header>
                 <main className="home">
                     <div className="homeOverLay">
+                   
+                    </div>
                         <Container className="homeText">
                         <h2 className="display-2 text-white">
                         Africa SDG Watch is a public platform to visualize and explore data, benchmark progress towards the 
@@ -24,17 +41,18 @@ class Home extends React.Component {
                                 <Link to="/Sdgs/Sdg_1" className="btn btn-warning text-white">Explore Development Data</Link>
                             </Col>
                             <Col md="3">
-                                <InputGroup className="mb-4">
+                                {/* <InputGroup className="mb-4">
                                     <InputGroupAddon addonType="prepend">
                                         <InputGroupText>
                                         <i className="ni ni-zoom-split-in" />
                                         </InputGroupText>
                                     </InputGroupAddon>
-                                    <Input placeholder="Search by country" type="text" />
-                                </InputGroup>
+                                     
+                                </InputGroup> */}
+                                <Select options={countries} placeholder="Search by country"/>
                             </Col>
                         </Row>
-                    </div>
+                    
                    
                 </main>
               
