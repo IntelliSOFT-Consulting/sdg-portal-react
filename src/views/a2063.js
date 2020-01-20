8a4492118e18613b192f6e226f59da7e9ef97dfb
Luciekimotho <luciekimotho@gmail.com>
2020-01-20 16:27:13 +0300

    Agenda 2063 added map

--- src/views/a2063.js
+++ src/views/a2063.js
@@ -2,28 +2,82 @@ import React, {useState, useEffect, useCallback} from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Map from "../visualizations/map";
+
 import classnames from "classnames";
 import {
     Row, Col, Nav,NavItem, NavLink, Card, TabContent, TabPane, Input, Button
 } from "reactstrap";
 
 
-const agenda2063 = require('../assets/data/agenda2063.json');
-//console.log(agenda2063Data)
-
 function A2063(){
-    const [activeTab, setActiveTab] = useState('1')
-    const [innerTab, setInnerTab] = useState('1')
+    const Papa = require("papaparse/papaparse.min.js");
+    const agenda2063 = require('../assets/data/agenda2063.json');
+    const a2063DataSource = require("../assets/data/sdg/pan.csv");
 
-    const targetClick = (e) =>{
-        console.log(e)
-        setActiveTab(e.target.value)
+    const [activeTab, setActiveTab] = useState(1);
+    const [innerTab, setInnerTab] = useState(1);
+
+    const [indicator, setIndicator] = useState(1);
+    const [source, setSource] = useState('gdb');
+    const [year, setYear] = useState('2006');
+    const [type, setType] = useState('map');
+    const [mapData, setMapData] = useState([]);
+    const years = [];
+
+
+    const formatData = (data) => {
+        let formattedData = [];
+        if(type == 'map'){
+            data.forEach(function(d){
+                if(d.Year === year){
+                    formattedData.push({
+                        "code": d.Code,
+                        "drilldown": d.Code,
+                        "value": d[indicator],
+                        "country": d.Entity
+                    })
+                }
+            })
+        }else if (type == 'chart'){
+            Object.defineProperty(Array.prototype, 'group', {
+                enumerable: false,
+                value: function (key) {
+                    let map = {};
+                    this.map(e => ({k: key(e), d: e})).forEach(e => {
+                    map[e.k] = map[e.k] || [];
+                    map[e.k].push(e.d);
+                    });
+                    return Object.keys(map).map(k => ({country: k, data: map[k]}));
+                }
+            });
+
+            formattedData = data.group(item => item.Entity)
+        }
+        return formattedData;
     }
 
-    const innerTargetClick = (e) => {
-        setInnerTab(e.target.value)
+    const setChartType = () => {
+        setType('chart');
+    }
+    const setMapType = () => {
+        setType('map');
     }
 
+    useEffect(() => {
+        const fetchA2063Data = (csvFile) => {
+            Papa.parse(csvFile, {
+                download: true,
+                header: true,
+                complete: function(results){
+                    console.log(results.data);
+                    const formattedData = formatData(results.data);
+                    console.log(formattedData);
+                    setMapData(formattedData);
+                }
+            })
+        }
+        fetchA2063Data(a2063DataSource);
+    }, [indicator, source, year, type])
 
     return (
         <>
@@ -57,7 +111,6 @@ function A2063(){
                                 goals = data.goals
                                 
                                 return <TabPane key={index} tabId={index+1} className={ activeTab === index+1 ? "active" : ""} >
-                                    {console.log(activeTab)}
                                             <div className="agenda2063Header">
                                                 <Col md="12">
                                                     <h5 className="display-4 mt-2 mb-2 text-center">
@@ -69,8 +122,8 @@ function A2063(){
                                                                 goals.map((goalData, index) =>{
                                                                 return <NavItem key={index}>
                                                                             <NavLink aria-selected={index+1 } 
-                                                                                className={classnames("btn btn-warning", { active: index+1 })}
-                                                                                onClick={innerTargetClick} href="#pablo" role="tab">
+                                                                                className={classnames("btn btn-warning", { active: innerTab === index+1 })}
+                                                                                onClick={  () =>setInnerTab(index+1)  } href="#!" role="tab">
                                                                                 Goal {goalData.number}
                                                                             </NavLink>
                                                                         </NavItem>
@@ -88,7 +141,7 @@ function A2063(){
                                                         let indicators = goalData.indicators;
                                                         let newID = index + 1
                                                         let tabId = "innerTabs"+newID
-                                                        return <TabPane key={index} tabId={tabId} className={activeTab}> 
+                                                        return <TabPane key={index} tabId={index+1} className={ innerTab === index+1 ? "active" : ""}> 
                                                                     <Card>
                                                                         <Row className="selectButtons mt-2 mb-2">
                                                                             <Col md="3">
@@ -111,8 +164,6 @@ function A2063(){
                                                                                 </Input>
                                                                             </Col>
                                                                         </Row>
-                                                                        {/* <Map></Map> */}
-
                                                                     </Card>
                                                                 </TabPane>
                                                     })
@@ -123,6 +174,23 @@ function A2063(){
                                     })
                                     }
                         </TabContent>
+                        <Card>
+                                 <Map mapData={mapData}></Map>
+                                 <div>
+                                    <Button color="primary" type="button" className={ classnames("btn-icon" , { active: type === 'map' })} onClick={setMapType}>
+                                        <span className="btn-inner--icon">
+                                            <i className="fa fa-globe" />
+                                        </span>
+                                        <span className="btn-inner--text">MAP</span>
+                                    </Button>
+                                    <Button color="primary" type="button" className={ classnames("btn-icon" , { active: type === 'chart' })}  onClick={setChartType}> 
+                                        <span className="btn-inner--icon">
+                                        <i className="fa fa-chart-bar"></i>
+                                        </span>
+                                        <span className="btn-inner--text">CHART</span>
+                                    </Button>
+                                </div>
+                        </Card>
                     </Col>
                 </Row> 
             </main>
