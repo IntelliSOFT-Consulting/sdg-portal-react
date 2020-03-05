89e272b13a46f1c411a43b49419baa624253491e
Luciekimotho <luciekimotho@gmail.com>
2020-03-05 12:37:42 +0300

    Added animation to img hover and underline text on hover  to about us page

--- src/views/About.js
+++ src/views/About.js
@@ -23,7 +23,7 @@ class About extends React.Component {
                 <main>
                 <div className="container-fluid pl-5 pr-5 pb-5">
                     
-                    <Row className="pl-5 pr-5">
+                    <Row className="pl-5 pr-5 pt-3">
                    
                     <p className="text-center mb-5">
                     The Portal aims to serve as a unique access point to timely and reliable information about SDG and 
