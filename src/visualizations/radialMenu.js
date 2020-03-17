5a936efdd7e5d9b2c891336bcadfa392f136f9b7
Lucy Kimotho <luciekimotho@gmail.com>
2020-03-17 10:35:08 +0300

    Dashboard conflict (#8)
    * Dashboard changes
    
    * Added scorecard data and styling on the dashboard page
    
    * Added Africa map to Country profile page
    
    * Added a modal to the country profile page
    
    * Changed Country profile to functional component and got modal to work
    
    * Country profile modal with SDG icons
    
    * Added gauge library
    
    * Country profile gauge working
    
    * Added country demographic
    
    * Styling of the demographics
    
    * Styling of the demographics
    
    * Agenda 2063 tabs
    
    * Added Agenda 2063 map
    
    * Sdg 1 page in progress
    
    * Added papa parse
    
    * Data being consumed in sdg1
    
    * Added useEffect and empty array parameter
    
    * Added home page header and content
    
    * Added home page navbar icons
    
    * Added country search on home page
    
    * Worked on changes from home icons
    
    * Country profile search working
    
    * Replaced var with let
    
    * Replaced var with let
    
    * Replaced var with let
    
    * Added tabs on SDG page
    
    * Added indicator data
    
    * Able to parse compiled data, renamed country codes to iso-2
    
    * Added a loading spinner
    
    * Different datasources
    
    * Indicator dropdown working correctly
    
    * Added drilldown, though not working
    
    * Recharts charts added
    
    * Added year range for x axis
    
    * CHart and map both showing
    
    * Added 2nd chart callback to allow both chart and map
    
    * Added high maps line chart
    
    * Added comments
    
    * Added cleanup function
    
    * Agenda 2063 hooks tabs working
    
    * Agenda 2063 added map
    
    * Added simple bar graph
    
    * Added exporting module
    
    * Replicated map and chart to a2063
    
    * Added drilldown
    
    * Changed the routing
    
    * Added map drilldown
    
    * Changed navigation
    
    * Fixed navigation
    
    * Merge
    
    * Fixed navigation, diffferent headers for pages
    
    * Made changes on the visualizations
    
    * Changed indicator code
    
    * Country profile changes to modal and demographics chart
    
    * Country profile changes to modal and demographics chart
    
    * Countries checkbox modal working
    
    * Rearranged buttons layout
    
    * Animated button in the home page
    
    * Added add country button in the line chart
    
    * Added hamburger menu and changes to country profile
    
    * added % in the barometer
    
    * Barometer working with different goals
    
    * Added chart cleanup function
    
    * Added SDGs header with icons
    
    * Added SDG, Targets, Indicators change
    
    * Returned buttons to togggle maps
    
    * Added spinner with SDG logo
    
    * Dashboard colors working
    
    * Finished up dashboard modal
    
    * Added cards to country profile modal
    
    * Added gauge
    
    * Added index radar data
    
    * Added SDG colors for the radar chart
    
    * On click in index map working correctly
    
    * Added SDG index wheel
    
    * Sdgs landing page
    
    * SDG changes
    
    * About us page changes
    
    * About us page changes
    
    * About us page changes
    
    * Country profile page changes
    
    * Country profile page changes
    
    * Country profile page changes
    
    * Country profile page changes
    
    * Country profile page changes
    
    * Added Dashboard data and home animation
    
    * Changed SDG logo to the one with black text
    
    * Added center header
    
    * Added links to about us page
    
    * Added animation to img hover and underline text on hover  to about us page
    
    * Correct colors for the icons
    
    * Replaced dashboard with transparent icons
    
    * Changed country names and width
    
    * Fixed navigation on the dashboard page
    
    * Changes on dashboard
    
    * Year widget
    
    * Added dashboard year widget
    
    * Added Agenda 2063 header
    
    * A2063 header changes done
    
    * Added tree map and african map for Agenda 2063
    
    * Added overall A2063 and visualizations
    
    * Radial menu
    
    * Dashboard merge conflict
    
    Co-authored-by: KelvinWanyama <kevmurumba@gmail.com>

---
+++ src/visualizations/radialMenu.js
@@ -0,0 +1,11 @@
+import React from "react";
+import $ from 'jquery';
+import '../radialMenu.js';
+import '../radialMenu.css'
+
+
+class RadialMenu extends React.Component{
+    
+}
+
+export default RadialMenu;
