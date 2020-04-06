4c38af85247913a472d06c7a73acab003c549f0b
Kelvin Murumba <kevmurumba@Kelvins-MacBook-Pro.local>
2020-04-06 22:37:57 +0300

    Added a basename

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   return (
 
     
-    <Router basename={'/'}>
+    <Router basename={'/sdgportal'}>
       <Switch>
             {/* <Route exact path="/Sdgs" component={Sdgs}></Route> */}
             <Route exact path="/Sdgs" component={Sdgs1}></Route>
