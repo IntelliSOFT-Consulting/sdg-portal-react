4be5a452793d59241627ccfd5048b045334ff7fc
Luciekimotho <luciekimotho@gmail.com>
2020-05-12 19:03:57 +0300

    Added modal and files

--- src/App.js
+++ src/App.js
@@ -26,10 +26,10 @@ import DataUpload from "./views/DataUpload";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab, faYoutube, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faGlobeAfrica, faChartBar, faChartLine, faCircle, faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
+import { faGlobeAfrica, faChartBar, faChartLine, faCircle, faCaretDown, faCaretUp, faFolder, faFile } from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(fab, faGlobeAfrica, faChartBar, faChartLine, faCircle, faFacebook, faYoutube, faTwitter, faCaretUp, faCaretDown)
+library.add(fab, faGlobeAfrica, faChartBar, faChartLine, faCircle, faFacebook, faYoutube, faTwitter, faCaretUp, faCaretDown, faFolder, faFile)
 
 function App() {  
   return ( 
--- src/views/DataUpload.js
+++ src/views/DataUpload.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Header from '../components/header';
 import axios from 'axios';
+import {
+    Container, Row, Col, Button, Modal, Card, CardBody, CardHeader
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 //const API_BASE = "http://localhost:3000"
 
 function DataUpload(){
      
     const [file, setFile] = useState(null);
+    const [files, setFiles] = useState([]);
+    const [toggleModal, setOpenModal] = useState(false);
+
     const API_BASE = "http://localhost:3000/api"
 
     const onClickHandler = () =>{
@@ -35,16 +43,119 @@ function DataUpload(){
     }
 
     useEffect(() => {
-        console.log(file)
-    })
+        const fetchData = async() =>{
+            const result = await axios(API_BASE+'/files');
+            setFiles(result.data.data);
+            console.log(result.data.data);
+        }
+        fetchData();
+    }, [])
+
 
     return (
         <>
         <Header></Header>
-        <input type="text" className="form-control" name="title"></input>
-        <input type="text" className="form-control" name="description" ></input>
-        <input type="file" className="form-control" name="data" onChange={(e) => setFile(e.target.files[0])} />
-        <input type="submit" className="btn" name="Upload" onClick={onClickHandler}></input>
+        <div className="container-fluid files-div" >
+            <Button className="btn-warning" onClick={ () => setOpenModal(true) }>Add new data</Button>
+            <Row>
+                <Col>
+                    <label>SDGs Data</label>
+                    {
+                        files.map(file => {
+                            let filePath = "file: //" +  file.data.file.path;
+                            return <div className="file-div" key={file.Id}>
+                                    <FontAwesomeIcon icon="file" size="lg"></FontAwesomeIcon>
+                                    <a href = {filePath} >{file.data.file.name} </a>
+                                </div>
+                        })
+                    }
+                </Col>
+
+                <Col>
+                    <label>Agenda 2063 Data</label>
+                    {
+                        files.map(file => {
+                            let filePath = "file: //" +  file.data.file.path;
+                            return <div className="file-div" key={file.Id}>
+                                    <FontAwesomeIcon icon="file" size="lg"></FontAwesomeIcon>
+                                    <a href = {filePath} >{file.data.file.name} </a>
+                                </div>
+                        })
+                    }
+                </Col>
+
+                <Col>
+                    <label>Dashboard Data</label>
+                    {
+                        files.map(file => {
+                            let filePath = "file: //" +  file.data.file.path;
+                            return <div className="file-div" key={file.Id}>
+                                    <FontAwesomeIcon icon="file" size="lg"></FontAwesomeIcon>
+                                    <a href = {filePath} >{file.data.file.name} </a>
+                                </div>
+                        })
+                    }
+                </Col>
+
+                <Col>
+                    <label>Country Profile Data</label>
+                    {
+                        files.map(file => {
+                            let filePath = "file: //" +  file.data.file.path;
+                            return <div className="file-div" key={file.Id}>
+                                    <FontAwesomeIcon icon="file" size="lg"></FontAwesomeIcon>
+                                    <a href = {filePath} >{file.data.file.name} </a>
+                                </div>
+                        })
+                    }
+                </Col>
+            </Row>
+
+            <Modal className="uploadFilesModal" isOpen={toggleModal} toggle={toggleModal}>
+                <div className="modal-header">
+                <h5 className="countryName" cssModule={{'modal-title': 'w-100 text-center'}}>Add new data</h5>
+                    <button aria-label="Close" className="close" data-dismiss="modal" type="button"
+                        onClick={ () => setOpenModal(false)} >
+                        <span aria-hidden={true}>×</span>
+                    </button>    
+                </div>
+                <div className="modal-body">
+                    <Row>
+                        <Col md="2">
+                            <label>Title</label>
+                        </Col>
+                        <Col>
+                            <input type="text" className="form-control" name="title"></input>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md="2">
+                        <label>Description</label>
+                        </Col>
+                        <Col>
+                            <input type="text" className="form-control" name="description"></input>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md="2">
+                        <label>File</label>
+                            
+                        </Col>
+                        <Col>
+                            {/* <label for="file-upload" className="custom-file-upload">Choose file to upload</label> */}
+                            <input type="file" id="file-upload" onChange={(e) => setFile(e.target.files[0])} />
+                        </Col>
+                    </Row>
+                        <input type="submit" className="btn btn-warning center" name="Upload" onClick={onClickHandler}></input>
+                       
+                </div>
+
+            </Modal>
+
+        </div>
+        
+       
+
         </>
     )
 
